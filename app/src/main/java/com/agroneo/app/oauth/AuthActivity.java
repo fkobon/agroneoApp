24b0390949ba159417c7241da4c491ace369cd90
tronic <root@page.live>
2018-05-18 14:13:48 +0400

    Account manager base

--- app/src/main/java/com/agroneo/app/oauth/AuthActivity.java
+++ app/src/main/java/com/agroneo/app/oauth/AuthActivity.java
@@ -3,11 +3,15 @@ package com.agroneo.app.oauth;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.agroneo.app.R;
+
 public class AuthActivity extends AppCompatActivity {
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.connect);
+
     }
 }
